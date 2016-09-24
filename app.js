e192f704a2919bee211a0560e12f9eb263900a18
Scott Endicott <sendicott1391@gmail.com>
2016-09-24 12:55:02 -0400

    Set up environment and skeleton

---
+++ app.js
@@ -0,0 +1,44 @@
+let app = angular.module('NewsTrackerApp', []);
+
+app.factory("NewsService", function ($http) {
+    let newsArray = [];
+
+    $http({
+        method: 'GET',
+        url: "http://puzzlegram.herokuapp.com/news",
+    }).then(function (news) {
+        console.log(news);
+        angular.copy(news.data.news, newsArray);
+        //     for (let i = 0; i < newsArray.length; i++) {
+        //         newsArray[i].starred = false;
+        //     }
+    });
+
+    console.log(newsArray);
+    return {
+        getNews: function () {
+            console.log(newsArray);
+            return newsArray;
+        },
+
+        // star: function (id) {
+        //     for (let i = 0; i < newsArray.length; i++) {
+        //         if (id === newsArray[i].id) {
+        //             newsArray[i].starred = true;
+        //             // newsArray[i].starred ? true : false
+        //         } else {
+        //             newsArray[i].starred = false;
+        //         }
+        //     }
+        // }
+
+    }
+});
+
+app.controller("ButtonController", function($scope) {
+
+});
+
+app.controller("DisplayController", function($scope) {
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let gulp = require('gulp');
+let browserify = require('gulp-browserify');
+let sass = require('gulp-sass');
+
+gulp.task('default', ['html', 'css', 'js']);
+
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('css', function() {
+    return gulp.src('style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('index.html', ['html']);
+    gulp.watch('*.js', ['js']);
+    gulp.watch('style.scss', ['css']);
+});
