33cde00e25acf40944c6d0858d62d518758a75ce
Scott Endicott <sendicott1391@gmail.com>
2016-09-24 18:16:13 -0400

    Right side news sections are populating and right side is scrollable

--- app.js
+++ app.js
@@ -7,32 +7,14 @@ app.factory("NewsService", function ($http) {
         method: 'GET',
         url: "http://puzzlegram.herokuapp.com/news",
     }).then(function (news) {
-        console.log(news);
         angular.copy(news.data.news, newsArray);
-        //     for (let i = 0; i < newsArray.length; i++) {
-        //         newsArray[i].starred = false;
-        //     }
     });
 
-    console.log(newsArray);
-    
     return {
         getNews: function () {
             console.log(newsArray);
             return newsArray;
         },
-
-        // star: function (id) {
-        //     for (let i = 0; i < newsArray.length; i++) {
-        //         if (id === newsArray[i].id) {
-        //             newsArray[i].starred = true;
-        //             // newsArray[i].starred ? true : false
-        //         } else {
-        //             newsArray[i].starred = false;
-        //         }
-        //     }
-        // }
-
     }
 });
 
@@ -40,6 +22,6 @@ app.controller("ButtonController", function($scope) {
 
 });
 
-app.controller("DisplayController", function($scope) {
-
+app.controller("DisplayController", function($scope, NewsService) {
+    $scope.news = NewsService.getNews();
 });
