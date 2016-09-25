a53979875b93de2721f50deee7d7865ee03fd459
Scott Endicott <sendicott1391@gmail.com>
2016-09-25 17:45:45 -0400

    Working on Bloomberg button

--- app.js
+++ app.js
@@ -9,22 +9,50 @@ app.factory("NewsService", function ($http) {
         url: "http://puzzlegram.herokuapp.com/news",
     }).then(function (news) {
         angular.copy(news.data.news, newsArray);
-        console.log("Testing: " + newsArray[0].published);
+        // console.log("Testing: " + newsArray[0].published);
     });
 
     return {
         getNews: function () {
-            console.log(newsArray);
+            // console.log(newsArray);
             return newsArray;
         },
+
+        getBloom: function () {
+
+            for (let i = 0; i < newsArray.length; i++) {
+                
+            }
+        }
     }
 });
 
 app.controller("NewsController", function($scope, NewsService) {
     $scope.news = NewsService.getNews();
     $scope.removeHidden = function () {
-        console.log("hidden");
-        let hiddenClass = document.querySelectorAll(".newsBox").classList
-        hiddenClass.remove("hidden");
+        let hiddenClass = document.querySelectorAll(".newsBox");
+        for (let i = 0; i < hiddenClass.length; i++) {
+            hiddenClass[i].classList.remove("hidden");    
+        }
     }
+
+    $scope.bloomFind = function () {
+        let hiddenClass = document.querySelectorAll(".newsBox");
+        for (let i = 0; i < hiddenClass.length; i++) {
+            hiddenClass[i].classList.remove("hidden");    
+        }
+        
+    }
+
+    $scope.hideIt = function () {
+        console.log($scope);
+        console.log($scope.news);
+    }
+
+    // if (document.querySelectorAll("#hideClick").checked) {
+    //     console.log("checked");
+    // }
+    // let hideClickArray = document.querySelectorAll("#hideClick").checked;
+    // console.log(hideClickArray);
+
 });
